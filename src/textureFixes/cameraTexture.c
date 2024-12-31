7527449c76814b99c78029ef8fb538a537daa2e8
PogChampGuy <Kuumba.Lambie@gmail.com>
2024-12-31 18:25:17 -0500

    page check  for camera texture trigger

---
+++ src/textureFixes/cameraTexture.c
@@ -0,0 +1,13 @@
+#include <object.h>
+#include "../practice.h"
+
+extern uint32_t swapTextureFlag;
+
+void CheckTexturePage(Object *objP)
+{
+    if (practice.page != 0)
+    {
+        swapTextureFlag = 1;
+    }
+    DeleteObject3(objP);
+}
